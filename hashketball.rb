159d4c58acce96c8e0e2933cc59ebb3bb10ba0ae
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-18 01:33:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -53,27 +53,8 @@ def game_hash
          :steals =>
          :blocks =>
          :slam_dunks =>
-        },
-        {:player_name =>
-         :number  =>
-         :shoe =>
-         :points =>
-         :rebounds =>
-         :assists =>
-         :steals =>
-         :blocks =>
-         :slam_dunks =>
-        },
-        {:player_name =>
-         :number  =>
-         :shoe =>
-         :points =>
-         :rebounds =>
-         :assists =>
-         :steals =>
-         :blocks =>
-         :slam_dunks =>
-        },
+        }
+ 
       ]
       
     },
