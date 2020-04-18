bfc758c7ecb92995df0d59ecec690c93ba580100
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-18 01:15:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,16 @@
 def game_hash
   {
-    :home => {:team_name, :colors, :players}
-    ,
-    :away => {:team_name, :colors, :players}
+    :home => {
+      :team_name => "team name", 
+      :colors => ["color1", "color2", "color3"], 
+      :players
+      
+    },
+    :away => {
+      :team_name, 
+      :colors, 
+      :players
+    }
+    
   }
 end
