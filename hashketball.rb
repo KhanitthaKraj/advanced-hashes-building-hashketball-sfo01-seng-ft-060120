7f58563767e235ebc69f43f64607fa10f669239a
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-18 01:31:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,7 @@ def game_hash
          :blocks => 1,
          :slam_dunks => 1
         },
-        {:player_name => "Reggie Evans"
+        {:player_name => "Reggie Evans",
          :number  =>
          :shoe =>
          :points =>
